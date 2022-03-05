1a0ffec09997e31d47e771acfe26dd384680d3b4
Yan Kahanov <kahanov.yan@gmail.com>
2022-03-05 16:13:16 +0500

    made calculator without functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles/index.scss';
+import './styles/calculator.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const buttonList = document.querySelector('.calculator__button-list');
+    const buttons = [
+        'C', '+/-', '%', '÷',
+        '7', '8', '9', 'x',
+        '4', '5', '6', '–',
+        '1', '2', '3', '+',
+        '.', '0', '⌫', '=',
+    ]
+
+    buttons.forEach((button, index) => {
+        const newButton = document.createElement("button")
+        newButton.innerText = button
+        newButton.classList.add('calculator__button-item')
+        if(index < 3) newButton.classList.add('calculator__button-item_secondary');
+        if((index + 1) % 4 === 0) newButton.classList.add('calculator__button-item_primary');
+
+        buttonList.appendChild(newButton)
+    })
+})
